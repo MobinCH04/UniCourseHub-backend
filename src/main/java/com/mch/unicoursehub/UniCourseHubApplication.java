96add1384bca27325060b0cc1752a398ee646d47
Mobin Cheshmberah <mobinch142@gmail.com>
2025-11-25 20:08:33 +0330

    Write init method.

--- src/main/java/com/mch/unicoursehub/UniCourseHubApplication.java
+++ src/main/java/com/mch/unicoursehub/UniCourseHubApplication.java
@@ -1,13 +1,22 @@
 package com.mch.unicoursehub;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class UniCourseHubApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UniCourseHubApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        // Set the default time zone
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tehran"));
     }
 
 }
