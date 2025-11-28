571943470f0f508d15b16ef1d55baafcf028499d
Mobin Cheshmberah <mobinch142@gmail.com>
2025-11-28 15:35:22 +0330

    Add builder to this entity.

--- src/main/java/com/mch/unicoursehub/model/entity/Course.java
+++ src/main/java/com/mch/unicoursehub/model/entity/Course.java
@@ -2,6 +2,7 @@ package com.mch.unicoursehub.model.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Course {
 
     @Id
