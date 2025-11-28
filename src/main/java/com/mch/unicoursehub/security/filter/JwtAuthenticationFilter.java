e850941237abca355d02934ea5ca00a4f818e2ba
Mobin Cheshmberah <mobinch142@gmail.com>
2025-11-28 15:38:45 +0330

    Rewrite this class.

--- src/main/java/com/mch/unicoursehub/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/mch/unicoursehub/security/filter/JwtAuthenticationFilter.java
@@ -2,12 +2,16 @@ package com.mch.unicoursehub.security.filter;
 
 import com.mch.unicoursehub.ConstVal;
 import com.mch.unicoursehub.model.entity.Token;
+import com.mch.unicoursehub.model.enums.TokenType;
 import com.mch.unicoursehub.repository.TokenRepository;
 import com.mch.unicoursehub.security.service.JwtService;
+import com.mch.unicoursehub.service.impl.TokenServiceImpl;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,73 +21,70 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
+import java.io.IOException;
 import java.util.Optional;
+import java.util.UUID;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Filter for authenticating JWT tokens and setting up the Spring Security context.
+ * This filter checks the Authorization header for a valid JWT token, verifies its validity,
+ * and sets up the authentication in the SecurityContextHolder if the token is valid.
+ */
 @Component
-@AllArgsConstructor
-public class  JwtAuthenticationFilter extends OncePerRequestFilter {
-
-
-    private final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
-    private final TokenRepository tokenRepository;
     private final HandlerExceptionResolver handlerExceptionResolver;
+    private final TokenServiceImpl tokenService;
 
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain) {
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
 
         try {
 
-            final String authHeader = request.getHeader(ConstVal.HEADER_AUTHORIZATION);
-            final String jwt;
-            String username = null;
-            final String uuid;
-
-            if (authHeader == null || !authHeader.startsWith(ConstVal.PREFIX_BEARER)) {
-                logger.info(" Token rejected... ");
+            // اجازه می‌ده مسیرهای auth بدون JWT عبور کنند
+            if (request.getServletPath().contains("/api/v1/auth")) {
                 filterChain.doFilter(request, response);
                 return;
             }
 
-            jwt = authHeader.substring(ConstVal.PREFIX_BEARER.length());
-            try {
-                username = jwtService.extractUsername(jwt);
-            } catch (ExpiredJwtException e) {
-                logger.error("token expired -> " + e.getMessage());
+            final String authHeader = request.getHeader("Authorization");
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ") || authHeader.length() <= 7) {
                 filterChain.doFilter(request, response);
                 return;
             }
-            uuid = jwtService.extractUUID(jwt);
 
-            if (username != null && uuid != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // گرفتن JWT از هدر
+            final String jwt = authHeader.substring(7);
+            final String userPhone = jwtService.extractUsername(jwt);
+            final String uuid = jwtService.extractUUID(jwt);
 
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (userPhone != null
+                    && SecurityContextHolder.getContext().getAuthentication() == null
+                    && uuid != null) {
 
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userPhone);
 
-                Optional<Token> token = tokenRepository.findByUuid(uuid);
-                if (token.isEmpty()){
-                    filterChain.doFilter(request, response);
-                return;
-            }
+                boolean isTokenValid = false;
+                Optional<Token> token = tokenService.checkToken(UUID.fromString(uuid), TokenType.ACCESS_TOKEN);
 
+                if (token.isPresent()) {
+                    isTokenValid = !token.get().isRevoked();
+                }
 
-                Token token1 = token.get();
-
-                boolean isTokenValid = !token1.isRevoked()
-                        && token1.getUser().getUsername().equals(username);
-
-                if (isTokenValid && jwtService.isTokenValid(jwt, userDetails)) {
-                    logger.info(" Token verified... ");
+                if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
                     UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                             userDetails,
                             null,
@@ -95,13 +96,12 @@ public class  JwtAuthenticationFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
             }
+
             filterChain.doFilter(request, response);
 
-        } catch (Exception ex) {
-
-            handlerExceptionResolver.resolveException(request, response, null, ex);
-
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            handlerExceptionResolver.resolveException(request, response, null, e);
         }
     }
-
 }
