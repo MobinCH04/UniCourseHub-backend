446b128ce351abad80d6d79bb80dde73b2f96df6
Mobin Cheshmberah <mobinch142@gmail.com>
2025-11-29 13:38:18 +0330

    Add getAllCourses method.

--- src/main/java/com/mch/unicoursehub/service/CourseService.java
+++ src/main/java/com/mch/unicoursehub/service/CourseService.java
@@ -1,8 +1,12 @@
 package com.mch.unicoursehub.service;
 
+import com.mch.unicoursehub.model.dto.AllCoursesResponse;
 import com.mch.unicoursehub.model.dto.CourseResponse;
 import com.mch.unicoursehub.model.dto.CreateCourseRequest;
+import com.mch.unicoursehub.utils.pagination.Pagination;
 
 public interface CourseService {
     CourseResponse createCourse(CreateCourseRequest request);
+
+    Pagination<AllCoursesResponse> getAllCourses(int page, int size, String code, String name, Integer unit);
 }
