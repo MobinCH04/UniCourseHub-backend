8ce36521a44c8babc7273e0b9e1d4356c67a1d08
Mobin Cheshmberah <mobinch142@gmail.com>
2025-11-29 15:10:12 +0330

    Add updateCourse method.

--- src/main/java/com/mch/unicoursehub/service/CourseService.java
+++ src/main/java/com/mch/unicoursehub/service/CourseService.java
@@ -3,10 +3,13 @@ package com.mch.unicoursehub.service;
 import com.mch.unicoursehub.model.dto.AllCoursesResponse;
 import com.mch.unicoursehub.model.dto.CourseResponse;
 import com.mch.unicoursehub.model.dto.CreateCourseRequest;
+import com.mch.unicoursehub.model.dto.UpdateCourseRequest;
 import com.mch.unicoursehub.utils.pagination.Pagination;
 
 public interface CourseService {
     CourseResponse createCourse(CreateCourseRequest request);
 
     Pagination<AllCoursesResponse> getAllCourses(int page, int size, String code, String name, Integer unit);
+
+    CourseResponse updateCourse(String code, UpdateCourseRequest req);
 }
