e306775552a2a37cf05be0a2aa4013a953dc7471
Mobin Cheshmberah <mobinch142@gmail.com>
2025-12-05 17:50:32 +0330

    Add findByPrerequisiteCid

--- src/main/java/com/mch/unicoursehub/repository/PrerequisiteRepository.java
+++ src/main/java/com/mch/unicoursehub/repository/PrerequisiteRepository.java
@@ -4,8 +4,12 @@ import com.mch.unicoursehub.model.entity.Prerequisite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PrerequisiteRepository extends JpaRepository<Prerequisite, UUID> {
+
+    List<Prerequisite> findByPrerequisiteCid(UUID cid);
+
 }
