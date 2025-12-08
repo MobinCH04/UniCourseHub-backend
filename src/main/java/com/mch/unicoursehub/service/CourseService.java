ccd82da59502395785ebe5e2f615959d221665db
Mobin Cheshmberah <mobinch142@gmail.com>
2025-12-08 20:16:07 +0330

    Replace CourseResponse with AllCoursesResponse.

--- src/main/java/com/mch/unicoursehub/service/CourseService.java
+++ src/main/java/com/mch/unicoursehub/service/CourseService.java
@@ -1,6 +1,5 @@
 package com.mch.unicoursehub.service;
 
-import com.mch.unicoursehub.model.dto.AllCoursesResponse;
 import com.mch.unicoursehub.model.dto.CourseResponse;
 import com.mch.unicoursehub.model.dto.CreateCourseRequest;
 import com.mch.unicoursehub.model.dto.UpdateCourseRequest;
@@ -9,7 +8,7 @@ import com.mch.unicoursehub.utils.pagination.Pagination;
 public interface CourseService {
     CourseResponse createCourse(CreateCourseRequest request);
 
-    Pagination<AllCoursesResponse> getAllCourses(int page, int size, String code, String name, Integer unit);
+    Pagination<CourseResponse> getAllCourses(int page, int size, String code, String name, Integer unit);
 
     CourseResponse updateCourse(String code, UpdateCourseRequest req);
 
