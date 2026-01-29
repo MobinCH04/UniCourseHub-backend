3a0485df387487e3a4b73b392f1dc903d0c8a99b
MightyKnightmare <sajad.var83@gmail.com>
2026-01-30 00:09:27 +0330

    create ProfessorController class

---
+++ src/main/java/com/mch/unicoursehub/controller/ProfessorController.java
@@ -0,0 +1,49 @@
+﻿package com.mch.unicoursehub.controller;
+
+import com.mch.unicoursehub.model.dto.CourseOfferingResponse;
+import com.mch.unicoursehub.model.dto.UserListResponse;
+import com.mch.unicoursehub.service.impl.ProfessorServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/professor")
+@RequiredArgsConstructor
+@Tag(name = "Professor", description = "Professor operations")
+public class ProfessorController {
+
+    private final ProfessorServiceImpl professorServiceImpl;
+
+    @Operation(summary = "Get course offerings assigned to the currently logged-in professor")
+    @GetMapping("/course-offerings")
+    public ResponseEntity<List<CourseOfferingResponse>> getMyCourseOfferings() {
+        List<CourseOfferingResponse> offerings = professorServiceImpl.getMyCourseOfferings();
+        return ResponseEntity.ok(offerings);
+    }
+
+    @Operation(summary = "Get students of a course offering assigned to the logged-in professor")
+    @GetMapping("/course-offerings/{courseOfferingId}/students")
+    public ResponseEntity<List<UserListResponse>> getStudentsOfCourseOffering(
+            @PathVariable UUID courseOfferingId) {
+
+        List<UserListResponse> students = professorServiceImpl.getStudentsOfCourseOffering(courseOfferingId);
+        return ResponseEntity.ok(students);
+    }
+
+    @Operation(summary = "Remove a student from a course offering (professor must own the offering)")
+    @DeleteMapping("/course-offerings/{courseOfferingId}/students/{studentId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void removeStudentFromCourseOffering(
+            @PathVariable UUID courseOfferingId,
+            @PathVariable UUID studentId) {
+
+        professorServiceImpl.removeStudentFromCourseOffering(courseOfferingId, studentId);
+    }
+}
